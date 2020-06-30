d30e631d06cbf712f43ac4d7b37710e32bdf926a
Bob Bill <bob.bill@mspbots.ai>
2020-06-30 14:57:55 +0800

    spring boot Upgrading 2.3.1 gradle upgrading 6.5

---
+++ bootiful-web/src/test/java/com/alexbob/web/BootifulWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexbob.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
